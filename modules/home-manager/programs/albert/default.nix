900edd33fceb19d546a03ded5bea490226b05ea1
Yushi Hattori <yushi.t.hattori@gmail.com>
2025-09-07 22:44:11 -0700

    Updated albert settings

--- modules/home-manager/programs/albert/default.nix
+++ modules/home-manager/programs/albert/default.nix
@@ -11,52 +11,87 @@
     # Source albert configuration from the home-manager store
     xdg.configFile."albert/config".text = ''
       [General]
-      frontend=widgetsboxmodel-ng
-      showTray=false
       telemetry=false
+
+      [application]
+      enabled=true
 
       [applications]
       enabled=true
-      global_handler_enabled=true
+
+      [caffeine]
+      enabled=false
+
+      [calculator_qalculate]
+      enabled=true
 
       [chromium]
-      enabled=true
-      fuzzy=false
-      global_handler_enabled=false
-      trigger=bm
+      enabled=false
 
       [clipboard]
       enabled=true
-      persistent=true
-      trigger=clipboard
+      history_length=25
+      persistent=false
 
-      [albert]
+      [datetime]
+      enabled=true
+      show_date_on_empty_query=true
+
+      [files]
       enabled=true
 
-      [calculator]
-      enabled=true
-
-      [debug]
+      [github]
       enabled=false
 
+      [hash]
+      enabled=false
+
+      [mediaremote]
+      enabled=true
+
       [path]
+      enabled=true
+
+      [snippets]
+      enabled=false
+
+      [spotify]
+      enabled=true
+
+      [ssh]
       enabled=false
 
       [system]
-      command_lock=loginctl lock-session
-      command_logout="[ \"$DESKTOP_SESSION\" = \"hyprland\" ] && { hyprctl -j clients 2>/dev/null | jq -j '.[] | \"dispatch closewindow address:\\(.address); \"' | xargs -r hyprctl --batch 2>/dev/null; } || [ \"$DESKTOP_SESSION\" = \"plasma\" ] && kdotool search '.*' windowclose %@ || true"
-      command_poweroff=systemctl poweroff -i
-      command_reboot=systemctl reboot -i
       enabled=true
-      logout_enabled=true
-      title_logout=Quit All Applications
-      title_poweroff=Shutdown
-      trigger=sys
+
+      [timer]
+      enabled=true
+
+      [timezones]
+      enabled=false
+
+      [vpn]
+      enabled=false
+
+      [websearch]
+      enabled=true
+
+      [widgetsboxmodel]
+      alwaysOnTop=true
+      clearOnHide=true
+      clientShadow=true
+      displayScrollbar=false
+      followCursor=true
+      hideOnFocusLoss=true
+      historySearch=true
+      itemCount=5
+      quitOnClose=false
+      showCentered=true
+      systemShadow=true
 
       [widgetsboxmodel-ng]
       alwaysOnTop=true
       clearOnHide=false
-      debug=false
       displayScrollbar=false
       followCursor=true
       hideOnFocusLoss=true
