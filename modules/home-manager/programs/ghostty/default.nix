222e34d21dcb1c5e718e1503f5baf2448eb379d2
Yushi Hattori <yushi.t.hattori@gmail.com>
2026-02-25 21:15:43 -0800

    Possible fix to ghostty monitor support

--- modules/home-manager/programs/ghostty/default.nix
+++ modules/home-manager/programs/ghostty/default.nix
@@ -5,6 +5,11 @@
     settings = {
       theme = "Catppuccin Mocha";
       command = "zellij attach --create main";
+
+      # Window management settings for multi-monitor support
+      window-save-state = "never";
+      gtk-single-instance = false;
+
       # font-size = 10;
       # keybind = [
       #   "ctrl+h=goto_split:left"
