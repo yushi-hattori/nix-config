f53e871393e805fbd2c3371928c2de260faa3724
Yushi Hattori <yushi.t.hattori@gmail.com>
2026-04-30 22:49:29 -0700

    added printer stuff

--- modules/nixos/services/printing/default.nix
+++ modules/nixos/services/printing/default.nix
@@ -6,15 +6,35 @@
     drivers = with pkgs; [
       gutenprint
       hplip
+      brlaser
+      splix
+      cups-bjnp
+      foomatic-db
+      foomatic-db-ppds
+      foomatic-db-nonfree
+      foomatic-db-engine
     ];
   };
 
   # Enable SANE for scanning
   hardware.sane = {
     enable = true;
-    extraBackends = [ pkgs.hplipWithPlugin ];
+    extraBackends = [
+      pkgs.hplipWithPlugin
+      pkgs.brscan4
+    ];
   };
 
   # System-wide packages for printer management
   programs.system-config-printer.enable = true;
+
+  # Additional desktop utilities for a "System Settings" feel
+  environment.systemPackages = with pkgs; [
+    blueman # Bluetooth manager
+    pavucontrol # Audio control
+    networkmanagerapplet # nm-connection-editor and nm-applet
+  ];
+
+  # Enable services for these utilities
+  services.blueman.enable = true;
 }
