9509e2f287f9d8f30d693c9613253902b1eee0b3
Yushi Hattori <yushi.t.hattori@gmail.com>
2026-06-16 23:03:39 -0700

    fixed niri toggle center

--- modules/home-manager/desktop/niri/toggle_center.sh
+++ modules/home-manager/desktop/niri/toggle_center.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 CENTER_FILE="$HOME/.config/niri/centering.kdl"
 
+# If it's a symlink (from Nix), remove it and create a real file
+if [ -L "$CENTER_FILE" ]; then
+    rm "$CENTER_FILE"
+    echo 'layout { center-focused-column "always"; }' > "$CENTER_FILE"
+fi
+
 if grep -q '"always"' "$CENTER_FILE" 2>/dev/null; then
     echo 'layout { center-focused-column "never"; }' > "$CENTER_FILE"
 else
